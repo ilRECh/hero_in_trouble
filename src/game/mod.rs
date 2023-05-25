bd06b8f3a97ffdc54fe773214ec7194a10605b03
ilRECh <ildar.rch@gmail.com>
2023-05-25 23:54:05 +0200

    Adding modules

---
+++ src/game/mod.rs
@@ -0,0 +1,5 @@
+
+
+pub fn game() {
+    println!("Hi!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-use ggez::*;
 
-struct State {
-    frame_delta_time: std::time::Duration,
-}
 
-impl ggez::event::EventHandler<GameError> for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        self.frame_delta_time = ctx.time.delta();
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        println!("Hello ggez! frame delta time = {}ms", self.frame_delta_time.as_millis());
-        Ok(())
-    }
-}
+pub mod game;
 
 fn main() {
-    let state = State {
-        frame_delta_time: std::time::Duration::new(0, 0),
-    };
-
-    let config = conf::Conf::new();
-    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "ilRECh")
-        .default_conf(config)
-        .build()
-        .unwrap();
-
-    event::run(ctx, event_loop, state);
+    game::game();
 }
-
-/*fn main(argc, argv) {
-    
-}*/
